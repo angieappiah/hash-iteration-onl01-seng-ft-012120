603ff050f56d1a407ac61034c2f1f0a9960b87a0
Angela Appiah <alegnalawn2@yahoo.com>
2020-01-26 04:10:15 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,13 +7,13 @@
 # }
 
 def select_winner(passengers)
-  winner = ""
+  winner = "name"
   passengers.each do
     |suite,name |
     if suite == : suite_a &&
       name.start_with?
       ("A") 
-      winner = name
-      end
+    end
+      
   # add the code snippet here!
 end
